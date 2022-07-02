cf876296e30518481ab648439bf7057b462794c0
aidana <78kuba00@gmail.com>
2022-07-02 17:12:35 +0600

    data and model added

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -1,3 +1,14 @@
 from django.db import models
 
-# Create your models here.
+
+class Comment(models.Model):
+    Statuses = (
+        ('active', 'Активно'),
+        ('blocked', 'Заблокировано'),
+    )
+    author = models.CharField(max_length=50)
+    email = models.EmailField(max_length=50)
+    message = models.TextField(max_length=255)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    status = models.CharField(max_length=7, choices=Statuses, default='active')
